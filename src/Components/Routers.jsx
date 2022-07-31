8522e5598fae24969ec7c507d62822414318ec20
Prashant-SL <pacchidon07@gmail.com>
2022-07-31 22:50:02 +0530

    Initial Setup with Material UI

--- src/Components/Routers.jsx
+++ src/Components/Routers.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Route, Routes } from "react-router";
-import DetailsPage from './DetailsPage';
-import Home from './Home';
+import ExerciseDetails from '../pages/ExerciseDetails';
+import Home from '../pages/Home';
+import Navbar from './Navbar';
+import Button from '@mui/material/Button';
+import { Box } from '@mui/system';
+
 
 function Routers() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/details" element={<DetailsPage />} />
-        </Routes>
+
+        <Box>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/details" element={<ExerciseDetails />} />
+            </Routes>
+        </Box>
     )
 }
+
+const styles = {
+    btn: {
+        color: "black"
+    }
+};
 
 export default Routers;
